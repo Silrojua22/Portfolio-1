d74d3b96db6cba4f1a968ef2d9714fde9198d06f
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2023-10-01 01:43:58 -0300

    Footer

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Nav from "./components/commonComponents/nav/nav";
 import Home from "./components/mainViewPortfolioFiles/home/home";
 import DetailProject from "./components/mainViewPortfolioFiles/detailProject/detailProject";
+import Footer from "./components/commonComponents/footer/footer";
 
 function App() {
   const location = useLocation();
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/detailProject/:id" element={<DetailProject />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
