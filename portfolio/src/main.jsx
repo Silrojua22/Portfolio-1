0fc7ce1c3bdf0ee6806f07f908ded14075b24e4f
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2024-01-13 02:04:52 -0300

    Project push 2

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../src/redux/store.js";
 import axios from "axios";
-// axios.defaults.baseURL = "https://portfolio-1-production.up.railway.app/";
-axios.defaults.baseURL = "http://localhost:3002";
+axios.defaults.baseURL = "https://portfolio-1-production.up.railway.app/";
+// axios.defaults.baseURL = "http://localhost:3002";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
