668072af87c08544523054b845969867e34bfaf3
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2024-05-02 19:31:03 -0300

    Deploy back

--- api/src/db.js
+++ api/src/db.js
@@ -1,21 +1,25 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize")
-const ProjectFunction = require("./model/Project.js")
+const { Sequelize } = require("sequelize");
+const ProjectFunction = require("./model/Project.js");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { DB_URL } = process.env;
 
-const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-    { logging: false }
-);
+const sequelize = new Sequelize(DB_URL, {
+  logging: false,
+  dialectModule: pg,
+});
 
-
+// const sequelize = new Sequelize(
+//     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+//     { logging: false }
+// );
 
 ProjectFunction(sequelize);
 
 const { Project } = sequelize.models;
 
 module.exports = {
-    sequelize,
-    ...sequelize.models
-}
+  sequelize,
+  ...sequelize.models,
+};
